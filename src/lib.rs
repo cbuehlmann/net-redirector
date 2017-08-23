9ad4f21fd6ed2ed0b2ac124b93a14d1eb4975e86
Christian Buehlmann <christian.buehlmann@scs.ch>
2017-08-23 21:01:15 +0200

    add initial structure

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
+
+mod net_interceptor {
+
+	mod tcp_server {
+	}
+	
+	mod udp_server {
+	}
+
+	mod configuration {
+	}
+}
+
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate net_interceptor;
+
+fn main() {
+	
+	println!("main()");
+
+}
